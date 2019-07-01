6ca863795cdb21f732781d6015e959fb2b687d21
Chloe Ashley <chloeashley193@gmail.com>
2019-07-01 12:24:02 +0100

    First Publish

---
+++ docs/precache-manifest.6db8eb95724f8e010f7bb5144c542930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c3473933fc2fa9a21b1bcaad36e4f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3724d6c0484c7171078f",
+    "url": "/static/css/main.db27cdf5.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3724d6c0484c7171078f",
+    "url": "/static/js/main.16afac12.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
